90823476e0b815d21d56dc2e34784389dd2bd96c
Admin <huutam.ptnk@gmail.com>
2019-08-01 22:14:07 +0300

    update ExceptionsStandard.cpp

---
+++ Exceptions/ExceptionsStandard.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class CanGoWrong {
+public:
+    CanGoWrong() {
+        char *pMemory = new char[99999999999999999999];
+        delete[] pMemory;
+    }
+};
+
+int main() {
+    try {
+        CanGoWrong wrong;
+    } catch (bad_alloc &e) {
+        cout << "Caught exception: " << e.what() << endl;
+    }
+
+    cout << "Still running" << endl;
+    return 0;
+}
