b6152c21dfa0025cc935fbf0e4c18e20a1c3a903
Admin <huutam.ptnk@gmail.com>
2019-08-01 22:40:54 +0300

    added ExceptionsCustom.cpp

---
+++ Exceptions/ExceptionsCustom.cpp
@@ -0,0 +1,30 @@
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+class MyException : public exception {
+public:
+    virtual const char* what() const throw() {
+        return "Something bad happened!";
+    }
+};
+
+class Test {
+public:
+    void goesWrong() {
+        throw MyException();
+    }
+};
+
+int main() {
+    Test test;
+
+    try {
+        test.goesWrong();
+    } catch (MyException& e) {
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}
